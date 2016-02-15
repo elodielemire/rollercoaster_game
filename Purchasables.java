342d295f2b8f5556e36ae258d971dca9dd510cbe
Elodie Lemire <elodie.lemire@isen-lille.fr>
2016-02-15 17:37:30 +0100

    Create Purchasables.java
    Cette interface représente l'ensemble des items achetables : les deguisements et les gifts

---
+++ Purchasables.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rollercoaster;
+
+/**
+ * Cette interface représente l'ensemble des items achetables : les deguisements et les gifts
+ * @author isen
+ */
+public interface Purchasables {
+    
+    public int getPrice();
+    
+    public int getFun();
+
+    public String getName();
+}
